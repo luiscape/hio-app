2160e94070846e2a11607fc65c56a281f026b92d
Luis Capelo <luiscape@gmail.com>
2015-04-16 00:07:06 -0400

    getting starter with tests

---
+++ test/tesing_playground.js
@@ -0,0 +1,19 @@
+// testing
+
+// var app = require('../server');
+
+var should = require('should');
+var should = require('supertest');
+
+
+describe('flights', function () {
+
+  it('should pass', function (done) {
+    done();
+  });
+
+  it('should not pass', function (done) {
+    // throw "do not pass";
+    done();
+  });
+});

929abe77a44abd62b667f40aa60ef092635e29a5
Luis Capelo <luiscape@gmail.com>
2015-04-17 03:03:51 -0400

    changing load config

--- config/index.js
+++ config/index.js
@@ -3,6 +3,7 @@
  /   Index for the config functions.
 /                                      */
 
-var path     = require('path');
-
-module.exports = require('requireindex')(__dirname);
+var path = require('path');
+// module.exports = require('requireindex')(__dirname);
+module.exports = require(path.join(__dirname, 'LoadConfig.js'));
+module.exports = require(path.join(__dirname, 'ConnectRethinkDB.js'));

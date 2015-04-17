0d4b0394503e1f3e075b8f55dbddea0befa55747
Luis Capelo <luiscape@gmail.com>
2015-04-17 00:45:14 -0400

    adding coveralls

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-istanbul cover _mocha -- -R spec
+# istanbul cover _mocha -- -R spec
+istanbul cover ./node_modules/mocha/bin/_mocha --report lcovonly -- -R spec && cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js && rm -rf ./coverage
 # open coverage/lcov-report/index.html

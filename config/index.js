04b3d5b49a45214335d99fe76366e5aa4fc02155
Luis Capelo <luiscape@gmail.com>
2015-04-17 03:09:05 -0400

    t

--- config/index.js
+++ config/index.js
@@ -3,7 +3,4 @@
  /   Index for the config functions.
 /                                      */
 
-var path = require('path');
-// module.exports = require('requireindex')(__dirname);
-module.exports = require(path.join(__dirname, 'LoadConfig.js'));
-module.exports = require(path.join(__dirname, 'ConnectRethinkDB.js'));
+module.exports = require('requireindex')(__dirname);
